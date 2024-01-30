496906ae437098c81695b045907484b8b04628c8
Kristina Skutilova <kristina.skutilova@ucdconnect.ie>
2024-01-30 10:25:18 +0000

    add entities interface

---
+++ grid/core/src/com/badlogic/game/Entities.java
@@ -0,0 +1,9 @@
+package com.badlogic.game;
+
+public interface Entities {
+    public void getCentre();
+    public float[] getCoordinates();
+    public void getPosition();
+    public void getCollision();
+    public void Draw();
+}
