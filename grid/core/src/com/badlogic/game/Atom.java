23e181d8a9ed2e229f61802e5c732adca585d684
Kristina Skutilova <kristina.skutilova@ucdconnect.ie>
2024-02-20 09:23:25 +0000

    comment Atom class

--- grid/core/src/com/badlogic/game/Atom.java
+++ grid/core/src/com/badlogic/game/Atom.java
@@ -3,20 +3,33 @@ package com.badlogic.game;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+
+//Atom implements Entities since it is an entity that is going to draw
+//implements Clickable since it can be clicked (maybe not? mostly because of the toggle really)
 public class Atom implements Entities, Clickable{
 
+    //X coordinate for the centre point of the atom
     private float atomCentreX;
+    //Y coordinate for the centre point of the atom
     private float atomCentreY;
+    //radius for the atom nucleus
     private float atomCentreRadius;
+    //radius for the field of influence
     private float atomLayerRadius;
 
     //a point on the layer that is directly to the right of the center
+    //Y coord is the same as for the center
+    //X coord is centreX + radius
     private float[] layerPoint = new float[2];
 
+    //array storing the centre of the atom in the sense that centre[X],[Y] is stored here
     private float[] centrePoint = new float[2];
     //maybe this could be made into a 2D array since storing point coordinates?
+    //array of all points in the atom
     private float[] atomPoints;
 
+
+    //initialise atom
     Atom(float x, float y, float r1, float r2) {
         this.atomCentreX = x;
         this.atomCentreY = y;
@@ -31,10 +44,14 @@ public class Atom implements Entities, Clickable{
 
     }
 
-    public float[] setAtomPoints(){
+
+    //setter method
+    public float[] setAtomPoints(float x1, float y1){
+        //takes the new centre, sets the centre to be the new centre
+        //sets the layer point to be x of new centre + radius, y of new centre
         float[] atomPoints = {
-                atomCentreX, atomCentreY,
-                this.layerPoint[0], this.layerPoint[1]
+                x1, y1,
+                x1+this.atomCentreRadius, y1
         };
         return atomPoints;
     }
@@ -104,9 +121,11 @@ public class Atom implements Entities, Clickable{
         int num = n;
         float centreX = this.getCentre()[0];
         float centreY = this.getCentre()[1];
+        //check if the number of atoms we want is correct
         if (num<5 || num>6){
             throw new IllegalArgumentException("You can only play with 5 or 6 atoms");
         }
+        //render 5 or 6 atoms to show how many atoms are left to be placed
         for(int i=0; i<num; i++){
             Atom a = new Atom(centreX, centreY, this.getCentreRadius(), this.getLayerRadius());
             a.Draw(shape);
