06d857848a8c58e3a4d0dbaacc64bb05ed0fd8b0
Cian Cockerill <ciancockerill@gmail.com>
2024-02-20 09:35:51 +0000

    Camera + Viewport for window resizing

--- grid/core/src/com/badlogic/game/Main.java
+++ grid/core/src/com/badlogic/game/Main.java
@@ -2,42 +2,39 @@ package com.badlogic.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.GL20;
-
-
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Main extends ApplicationAdapter {
 
     ShapeRenderer shape;
-    Hexagon hex;
-    public static float windowWidth;
-    public static float windowHeight;
-    public static float hexRadius;
-    private Stage stage;
-    private Skin skin;
+    HexagonGrid hex;
+
+    public static int windowWidth = 1600;
+    public static int windowHeight = 900;
+    public static float hexRadius = 50;
+
+    private Viewport viewport;
+    private Camera cam;
 
     @Override
     public void create () { // on start
+        float scrW = Gdx.graphics.getWidth();
+        float scrH =  Gdx.graphics.getHeight();
+
+        cam = new PerspectiveCamera();
+        viewport = new FitViewport(windowWidth, windowHeight, cam);
+        cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
+        cam.update();
+
         shape = new ShapeRenderer();
-        hex = new Hexagon(300,300,250);
-
-        stage = new Stage();
-        Gdx.input.setInputProcessor(stage);
-
-        skin = new Skin(Gdx.files.internal("rainbow/skin/rainbow-ui.json"));
-        // Create a dialog with a message
-        Dialog dialog = new Dialog("Message", skin);
-        dialog.text("Welcome, time traveller!");
-        dialog.button("fuck off fr", true); // Add an OK button
-        dialog.show(stage);
+        hex = new HexagonGrid();
+        hex.buildHexBoard();
     }
 
     @Override
@@ -46,10 +43,14 @@ public class Main extends ApplicationAdapter {
         // ------ Update ------
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        cam.update();
 
         hex.Draw(shape);
+        hex.update();
+    }
 
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 
     @Override
