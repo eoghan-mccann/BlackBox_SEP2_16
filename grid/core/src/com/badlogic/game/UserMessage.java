98d7480f6b5acdc1b24f21cb62fd955e6b15d5d8
Kristina Skutilova <kristina.skutilova@ucdconnect.ie>
2024-02-20 10:05:43 +0000

    user message class add

--- grid/core/src/com/badlogic/game/UserMessage.java
+++ grid/core/src/com/badlogic/game/UserMessage.java
@@ -1,4 +1,24 @@
 package com.badlogic.game;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
 public class UserMessage {
+    private Stage stage;
+    private Skin skin;
+
+    public UserMessage(Stage stage, Skin skin) {
+        this.stage = stage;
+        this.skin = skin;
+    }
+
+    public void showMessage(String title, String message) {
+        Dialog dialog = new Dialog(title, skin);
+        dialog.text(message);
+        dialog.button("OK");
+        dialog.show(stage);
+    }
 }
+
