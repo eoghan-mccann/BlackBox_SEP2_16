76fbc20ce868a03934f57513c8e3770c4a67703d
Eoghan McCann <eoghan.mccann@ucdconnect.ie>
2024-02-23 10:17:25 +0000

    Added another ray class i'm working on. Border hexagons are a different color when you run

---
+++ grid/core/src/com/badlogic/game/Ray2.java
@@ -0,0 +1,30 @@
+package com.badlogic.game;
+
+public class Ray2 {
+    /*
+    Alternate ray idea
+     */
+
+    enum Direction  { // enum of ray's directions
+        NE,
+        E,
+        SE,
+        SW,
+        W,
+        NW,
+    }
+
+    Direction direction;
+
+    // Potentially take chosen side coords, determine direction, on update displace
+    public Ray2(float x1, float y1, float x2, float y2) {
+
+
+    }
+
+
+    public void setDirection(float x1, float y1, float x2, float y2)
+    {
+
+    }
+}
