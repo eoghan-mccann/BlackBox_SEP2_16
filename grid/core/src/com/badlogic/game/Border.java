f8bbc447319b7fef2fde0d07fe876e3d8cd51176
Cian Cockerill <ciancockerill@gmail.com>
2024-03-06 13:26:57 +0000

    Debug button working with rays

--- grid/core/src/com/badlogic/game/Border.java
+++ grid/core/src/com/badlogic/game/Border.java
@@ -166,7 +166,7 @@ public class Border implements Clickable, Entities{
 
     @Override
     public void update() {
-        if (isClicked() && !hasRay) { // Various pixel offsets due to the
+        if (isClicked() && !hasRay) { // Various pixel offsets due to the isInside function
             if (direction == Ray2.Direction.NE || direction == Ray2.Direction.NW) {
                 hexagonGrid.addRay(midPoint[0], midPoint[1] - 1, direction);
             } else if (direction == Ray2.Direction.SE){
